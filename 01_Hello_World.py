0406bc679ce783c021903a67fbe0806a186de6c1
A693203 <bbb@wp.pl>
2019-09-23 14:49:50 +0200

    bbb

--- 01_Hello_World.py
+++ 01_Hello_World.py
@@ -1,1 +1,3 @@
 print("My first simple Python script!")
+
+
--- 02_Strings.py
+++ 02_Strings.py
@@ -1,1 +1,30 @@
-print("")
+print("This is a line of text!")
+print("This is first line!\n This is second line!")
+print("This is some \" text")
+
+#This string is stored in a variable
+phrase = "String in a variable"
+print(phrase)
+print("\n" + phrase + " and concatenated string")
+
+print(phrase.capitalize())
+
+print(phrase.upper())
+
+print(phrase.isupper())
+print(phrase.islower())
+
+# converts variable to uppercase and checks if its true
+print(phrase.upper().isupper())
+
+print(len(phrase))
+print(phrase[2])
+
+print(phrase.index("S"))
+print(phrase.index("i"))
+
+print(phrase)
+print(phrase.replace("variable", "variable_replaced"))
+
+.replace("value", "value")
+
