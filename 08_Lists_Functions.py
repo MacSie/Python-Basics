61f00048039e2139d3bcb69f09dc54db4c22c67b
A693203 <bbb@wp.pl>
2019-09-24 10:17:33 +0200

    bbb

---
+++ 08_Lists_Functions.py
@@ -0,0 +1,41 @@
+lucky_numbers = [4, 8, 9, 18, 42, 56]
+friends = ["Kevin", "Jack", "Jane", "Oscar", "Toby"]
+print(friends)
+print(lucky_numbers)
+
+friends.extend(lucky_numbers)
+print(friends)
+
+lucky_numbers = [4, 8, 9, 18, 42, 56]
+friends = ["Kevin", "Jack", "Jane", "Oscar", "Toby"]
+
+friends.append("Olek")
+print(friends)
+
+friends.insert(1, "Aleksander")
+print(friends)
+
+friends.remove("Olek")
+print(friends)
+
+friends.clear()
+print(friends)
+
+lucky_numbers = [4, 8, 9, 18, 42, 56]
+friends = ["Kevin", "Jack", "Jane", "Oscar", "Toby"]
+
+friends.pop()
+print(friends)
+
+print(friends.index("Jane"))
+
+print(friends.count("Jim"))
+
+friends.sort()
+lucky_numbers.sort()
+
+friends.reverse()
+lucky_numbers.reverse()
+
+friends2 = friends.copy()
+print(friends2)
